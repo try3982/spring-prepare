0256bfa810f7bd0c4f588f19a5a2c6b46058b3ff
WonJin <try3982@kakao.com>
2025-09-16 10:11:11 +0900

    프로젝트 생성

---
+++ src/main/java/com/sparta/springspprepare/SpringspPrepareApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springspprepare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringspPrepareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringspPrepareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/springspprepare/SpringspPrepareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springspprepare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringspPrepareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
